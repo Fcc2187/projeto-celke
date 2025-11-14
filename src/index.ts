ea80abce4c7f9d9422ab085166ea7b0dc62d5656
Felipe Cavalcanti Caminha <felipe.ccaminha@liga.pe.gov.br>
2025-11-14 14:40:28 -0300

    alguns ajustes com cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, {type Request,type Response} from "express";
 import UsersController from "./controllers/UsersController.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", UsersController);
 
